27c6656ea5f39681d7644c3933ad6e971b1d3365
lastp <lastp@0743269d-6a3c-46d9-8a30-58e9c168f05a>
2014-08-13 20:44:32 +0000

    Catalan translation

--- trunk/source/game.cpp
+++ trunk/source/game.cpp
@@ -1,5 +1,5 @@
 /*
-(C) 2000-2011  Petr Lastovicka
+(C) 2000-2014  Petr Lastovicka
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License.
--- trunk/source/protocol.cpp
+++ trunk/source/protocol.cpp
@@ -1,5 +1,5 @@
 /*
-(C) 2004-2011  Petr Lastovicka
+(C) 2004-2014  Petr Lastovicka
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License.
