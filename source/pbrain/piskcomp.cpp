ebf23187cc6af8b30f746128bc00744da9c776e0
Petr Laštovička <plastovicka@users.noreply.github.com>
2015-05-24 21:38:46 +0000

    version 8.7

--- source/pbrain/piskcomp.cpp
+++ source/pbrain/piskcomp.cpp
@@ -1,5 +1,5 @@
 /*
-	(C) 2000-2005  Petr Lastovicka
+	(C) 2000-2015  Petr Lastovicka
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License.
