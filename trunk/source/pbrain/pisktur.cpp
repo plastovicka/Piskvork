894e8473fd9ef273e09bff8d35fb578bac3137ab
lastp <lastp@0743269d-6a3c-46d9-8a30-58e9c168f05a>
2016-04-12 08:48:20 +0000

    removed message renju rule is not supported

--- trunk/source/pbrain/pisktur.cpp
+++ trunk/source/pbrain/pisktur.cpp
@@ -88,12 +88,6 @@ void computer()
 	DWORD t0, t1;
 	Psquare p;
 
-	static bool renjuErr = false;
-	if(info_renju && !renjuErr){
-		renjuErr = true;
-		pipeOut("MESSAGE renju rule is not supported");
-	}
-
 	resultMove=0;
 	holdMove=0;
 	highestEval=0;
